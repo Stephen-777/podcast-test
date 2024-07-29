9e05f0c72da2b9d9caef2549f8751d4a10d7e7d0
Stephen-777 <stephen_rupp@yahoo.com>
2024-07-29 05:17:47 +0000

    new update

--- feed.py
+++ feed.py
@@ -30,9 +30,8 @@ for item in yaml_data['item']:
     xml_tree.SubElement(item_element, 'description').text = item['description']
     xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
     xml_tree.SubElement(item_element, 'pubDate').text = item['published']
-    xml_tree.SubElement(item_element, 'title').text = item['title']
 
-enclosure = xml_tree.SubElement(item_element, 'enclsoure', {
+enclosure = xml_tree.SubElement(item_element, 'enclosure', {
     'url': link_prefix + item['file'],
     'type': 'audio/mpeg',
     'length': item['length']
